e82291c59552607d9acbfd2c44db39d62d5a71fc
JohnnyBoii909 <1018518@student.sae.edu.au>
2024-07-06 18:03:23 +1000

    Pushing the audio visualiser stuff

---
+++ Assets/Team/John/AudioPeer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioPeer : MonoBehaviour
+{
+    public AudioSource _audioSource;
+    public float[] samples = new float[512];
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetSpectrumAudioSource();
+
+        if (Input.GetKeyDown("s"))
+        {
+            PlayAudio();
+        }
+    }
+
+    void GetSpectrumAudioSource()
+    {
+        _audioSource.GetSpectrumData(samples, 0, FFTWindow.Blackman);
+    }
+
+    void PlayAudio()
+    {
+        _audioSource.Play();
+    }
+}
