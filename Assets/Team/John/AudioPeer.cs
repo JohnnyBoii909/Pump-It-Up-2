f888d5a5206ae5599675ca69986b4a8bdd3720d1
JohnnyBoii909 <1018518@student.sae.edu.au>
2024-07-08 22:52:09 +1000

    Spectrum Data and Banded Data audio visualisers
    The spectrum data is all the audio from 0-20,000 hertz turned into 512 channels then used to have the 512 cubes react to it, the Banded data subtracts that 20,000 hertz frequency into 8 bands of frequency ranges, then averages each band's data to affect the scale of the banded visualer in the front of the player, the 8 cubes.

--- Assets/Team/John/AudioPeer.cs
+++ Assets/Team/John/AudioPeer.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,22 +7,20 @@ using UnityEngine;
 public class AudioPeer : MonoBehaviour
 {
     public AudioSource _audioSource;
-    public float[] samples = new float[512];
-    
+    public static float[] samples = new float[512];
+    public static float[] _freqBand = new float[8];
+
     // Start is called before the first frame update
     void Start()
     {
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         GetSpectrumAudioSource();
-
-        if (Input.GetKeyDown("s"))
-        {
-            PlayAudio();
-        }
+        MakeFrequencyBands();
     }
 
     void GetSpectrumAudioSource()
@@ -29,8 +28,55 @@ public class AudioPeer : MonoBehaviour
         _audioSource.GetSpectrumData(samples, 0, FFTWindow.Blackman);
     }
 
-    void PlayAudio()
+    void MakeFrequencyBands()
     {
-        _audioSource.Play();
+        /*
+         * 22050 / 512 = 43hertz per sample
+         *
+         * 20-60hertz
+         * 60-250hertz
+         * 250-500hertz
+         * 500-2000hertz
+         * 2000-4000hertz
+         * 4000-6000hertz
+         * 6000-20000hertz
+         *
+         * 8 Bands
+         *
+         * 0 - 2 samples = 86hertz
+         * 1 - 4 samples = 172hertz, Adding the last Hertz to this hertz gives us a range of 87-258 hertz
+         * 2 - 8 samples = 344hertz, range = 259-602 hertz, As we go we are adding the hertz of all previous tiers
+         * 3 - 16 samples = 688Hertz, range = 345-1290 hertz
+         * 4 - 32 samples = 1378hertz, range = 1291-2666 hertz
+         * 5 - 64 samples = 2752hertz, range = 2667-5418 hertz
+         * 6 - 128 samples = 5504hertz, range = 5419-10922 hertz
+         * 7 - 256 samples - 11008hertz, range = 10923-21930 hertz
+         * 510 add 2 more and we will have our desired 512 channels
+         */
+        
+        //This loops through and powers all the samples by 2 to set up
+        int count = 0;
+        
+        for (int i = 0; i < 8; i++)
+        {
+            float average = 0;
+            int sampleCount = (int) Mathf.Pow(2, i) * 2;
+
+            if (i == 7)
+            {
+                sampleCount += 2;
+            }
+
+            for (int j = 0; j < sampleCount; j++)
+            {
+                average += samples[count]*(count + 1);
+                count++;
+            }
+
+            average /= count;
+
+            _freqBand[i] = average * 10;
+        }
+        
     }
 }
---
+++ Assets/Team/John/InstantiateCubes.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateCubes : MonoBehaviour
+{
+    public GameObject sampleCubePrefab;
+
+    private GameObject[] _sampleCube;
+
+    public int numberOfSamlpes;
+
+    public float maxScale;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _sampleCube = new GameObject[numberOfSamlpes];
+        for (int i = 0; i < numberOfSamlpes; i++)
+        {
+            GameObject _instanceSampleCube = Instantiate(sampleCubePrefab);
+            _instanceSampleCube.transform.position = transform.position;
+            _instanceSampleCube.transform.parent = transform;
+            _instanceSampleCube.name = "SampleCube" + i;
+            transform.eulerAngles = new Vector3(0, -0.703125f * i, 0);
+            _instanceSampleCube.transform.position = (Vector3.forward * 100) + new Vector3(0,20,0);
+            _sampleCube[i] = _instanceSampleCube;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < 512; i++)
+        {
+            if (_sampleCube != null)
+            {
+                _sampleCube[i].transform.localScale = new Vector3(1,(AudioPeer.samples[i]*maxScale)*2, 1);
+            }
+        }
+    }
+}
