1bbef8bc9e8baf2d76649e1852b47a3db837b31a
JohnnyBoii909 <1018518@student.sae.edu.au>
2024-07-15 15:40:57 +1000

    Current Changes push for .apk

---
+++ Assets/Team/John/TurnOffPodium.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOffPodium : MonoBehaviour
+{
+    public GameObject podium;
+
+    void TurnOffThePodiumView()
+    {
+        podium.SetActive(false);
+    }
+}
