16ea08a7f868f293e5369671b704456a8e6b92fc
JohnnyBoii909 <1018518@student.sae.edu.au>
2024-07-30 13:33:48 +1000

    Lots of changes happen

---
+++ Assets/Team/John/HighpassAudioFilter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighpassAudioFilter : MonoBehaviour
+{
+    void ApplyLowPassFilter()
+    {
+        AudioHighPassFilter highPassFilter = gameObject.AddComponent<AudioHighPassFilter>();
+        highPassFilter.cutoffFrequency = 500.0f; 
+    }
+}
---
+++ Assets/Team/John/LowpassAudioFilter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LowpassAudioFilter : MonoBehaviour
+{
+    void ApplyLowPassFilter()
+    {
+        AudioLowPassFilter lowPassFilter = gameObject.AddComponent<AudioLowPassFilter>();
+        lowPassFilter.cutoffFrequency = 1000.0f;
+    }
+}
