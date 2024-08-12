f105f70f71c471d9a5043113a2cdbd1dedf6f173
JohnnyBoii909 <1018518@student.sae.edu.au>
2024-08-12 17:44:28 +1000

    Added Input Recording
    My last atempt at binding the filtering to the vr controller was not a success, therefore my plan now is to troubleshoot the problem, so I made a input recorder to see what the inputs are getting recorded as

---
+++ Assets/Team/John/InputRecorder.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class InputRecorder : MonoBehaviour
+{
+    public Text displayText; // Assign in the Unity Inspector
+    private List<string> inputLog = new List<string>();
+
+    void Update()
+    {
+        foreach (KeyCode key in System.Enum.GetValues(typeof(KeyCode)))
+        {
+            if (Input.GetKeyDown(key))
+            {
+                string inputString = key.ToString();
+                inputLog.Insert(0, inputString); // Insert at the top of the list
+                UpdateDisplay();
+            }
+        }
+    }
+
+    void UpdateDisplay()
+    {
+        displayText.text = string.Join("\n", inputLog.ToArray());
+    }
+}
